72d547604f9ec0d527dfe5e63b238a4d973b8856
Erkan_filiz <erkanfiliz1715@gmail.com>
2023-11-21 23:53:40 +0100

    react-context-app3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer"
+import Navs from "./components/Navs"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import People from "./pages/People"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import PersonDetail from "./pages/PersonDetail"
+import Login from "./pages/Login"
+import LoginProvider from "./context/LoginProvider"
+import PrivateRouter from "./pages/PrivateRouter"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoginProvider>
+      <BrowserRouter>
+        <Navs />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="people" element={<PrivateRouter />}>
+            <Route path="" element={<People />} />
+            <Route path=":id" element={<PersonDetail />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </LoginProvider>
+  )
 }
 
-export default App;
+export default App
